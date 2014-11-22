68716887723ddd5921198cdf5fef3bcc6234c83a
maehshvora <maheshvorahelp@gmail.com>
2014-11-22 09:33:59 +0530

    getdata function defination

--- studentresult.c
+++ studentresult.c
@@ -4,12 +4,24 @@
 
 class stud
 {
-	int getdata()
-	{	
+	string name;
+	int sub1,sub2,sub3;
+	void getdata()
+	{
+		cout<<"Enter the student name";		
+		cin>>name;
+		cout<<"Enter the subject 1 marks : ";
+		cin>>sub1;
+		cout<<"Enter the subject 2 marks : ";
+		cin>>sub2;
+		cout<<"Enter the subject 3 marks : ";
+		cin>>sub3;
+		this.name=name;
+		this.sub1=sub1;
+		this.sub2=sub2;
+		this.sub3=sub3;
 	}	
 }
-
-
 
 void main()
 {
